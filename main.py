cc8c90f775ef127f46a74be0ba6ff16df1ab4641
lautis1 <lautaro.stroia@gmail.com>
2021-12-12 03:49:48 -0300

    increasing coverage

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi import HTTPException
 from infrastructure.routes import metrics_router
+from exceptions.auth_error import AuthorizationException
 
 app = FastAPI(title = "Ubademy - Metrics service",
             description = "Metrics service API")
+
+
+@app.exception_handler(AuthorizationException)
+async def auth_exception_handler(request, exc):
+    error = {"message": exc.detail}
+    return JSONResponse(status_code = exc.status_code, content = error)
+
 
 app.include_router(metrics_router.router, prefix='/metrics', tags=['metrics'])
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -12,6 +12,17 @@ header = {"apikey": os.getenv('API_KEY')}
 class CoursesMetricsTest(TestCase):
 
     @mock.patch("application.use_cases.courses.get.get_metrics")
+    def test_courses_without_apikey(self, mock_get):
+
+        mock_get.return_value.status_code = 401
+        response = test_app.get("/metrics/courses/")
+        response_json = response.json()
+
+        assert response.status_code == 401
+        assert response_json["message"] == "Error with API Key"
+
+
+    @mock.patch("application.use_cases.courses.get.get_metrics")
     def test_courses_metrics(self, mock_get):
 
         mock_get.return_value.status_code = 200
@@ -30,7 +41,7 @@ class CoursesMetricsTest(TestCase):
         }
         response = test_app.get("/metrics/courses/", headers = header)
         response_json = response.json()
-        
+      
         assert response.status_code == 200
         assert response_json == {
             "courses_amount": 2,
@@ -67,7 +78,7 @@ class CoursesMetricsTest(TestCase):
         }
         response = test_app.get("/metrics/courses/"+course_id, headers = header)
         response_json = response.json()
-  
+        
         assert response.status_code == 200
         assert response_json == {
             "course_id": course_id,
@@ -84,8 +95,21 @@ class CoursesMetricsTest(TestCase):
         }
 
 
+    @mock.patch("application.use_cases.courses.get.get_metrics_by_id")
+    def test_course_metrics_inexistent(self, mock_get):
+
+        course_id = "278f685b-69e2-4ad2-9d23-b64f971cf871"
+        mock_get.return_value.status_code = 200
+        
+        response = test_app.get("/metrics/courses/"+course_id, headers = header)
+        response_json = response.json()
+        
+        assert response.status_code == 200
+        assert response_json == {}        
+
+
     @mock.patch("application.use_cases.courses.get.get_metrics")
-    def test_courses_metrics_filtered(self, mock_get):
+    def test_courses_metrics_filtered_both(self, mock_get):
 
         mock_get.return_value.status_code = 200
         mock_get.return_value = {
@@ -119,6 +143,81 @@ class CoursesMetricsTest(TestCase):
                 "passed_exams": 3
             }
         }
+
+    
+    @mock.patch("application.use_cases.courses.get.get_metrics")
+    def test_courses_metrics_filtered_cat(self, mock_get):
+
+        mock_get.return_value.status_code = 200
+        mock_get.return_value = {
+            "courses_amount": 2,
+            "metrics": {
+                "total_users": 12,
+                "users_approved": 3,
+                "users_currently_studying": 9,
+                "exams_amount": 3,
+                "average_score": 0,
+                "approval_rate": 1,
+                "graded_exams": 3,
+                "passed_exams": 3
+            }
+        }
+        response = test_app.get("/metrics/courses/?category[]=1",
+                                headers = header)
+        response_json = response.json()
+        
+        assert response.status_code == 200
+        assert response_json == {
+            "courses_amount": 2,
+            "metrics": {
+                "total_users": 12,
+                "users_approved": 3,
+                "users_currently_studying": 9,
+                "exams_amount": 3,
+                "average_score": 0,
+                "approval_rate": 1,
+                "graded_exams": 3,
+                "passed_exams": 3
+            }
+        }
+
+
+    @mock.patch("application.use_cases.courses.get.get_metrics")
+    def test_courses_metrics_filtered_subs(self, mock_get):
+
+        mock_get.return_value.status_code = 200
+        mock_get.return_value = {
+            "courses_amount": 2,
+            "metrics": {
+                "total_users": 12,
+                "users_approved": 3,
+                "users_currently_studying": 9,
+                "exams_amount": 3,
+                "average_score": 0,
+                "approval_rate": 1,
+                "graded_exams": 3,
+                "passed_exams": 3
+            }
+        }
+        response = test_app.get("/metrics/courses/?subscription_type[]=free",
+                                headers = header)
+        response_json = response.json()
+        
+        assert response.status_code == 200
+        assert response_json == {
+            "courses_amount": 2,
+            "metrics": {
+                "total_users": 12,
+                "users_approved": 3,
+                "users_currently_studying": 9,
+                "exams_amount": 3,
+                "average_score": 0,
+                "approval_rate": 1,
+                "graded_exams": 3,
+                "passed_exams": 3
+            }
+        }
+
     
     @mock.patch("application.use_cases.users.get.get_metrics")
     def test_users_metrics(self, mock_get):
@@ -149,6 +248,18 @@ class CoursesMetricsTest(TestCase):
                 "times_pw_changed": 3
             }
         }
+
+
+    @mock.patch("application.use_cases.users.get.get_metrics")
+    def test_users_without_apikey(self, mock_get):
+
+        mock_get.return_value.status_code = 401
+        response = test_app.get("/metrics/users/")
+        response_json = response.json()
+
+        assert response.status_code == 401
+        assert response_json["message"] == "Error with API Key"
+
 
     @mock.patch("application.use_cases.users.get.get_metrics_by_id")
     def test_user_metrics_by_id(self, mock_get):
@@ -201,3 +312,17 @@ class CoursesMetricsTest(TestCase):
                 "total_money_sent": 0.04301
             }
         }
+
+
+    @mock.patch("application.use_cases.payments.get.get_metrics")
+    def test_payments_without_apikey(self, mock_get):
+
+        mock_get.return_value.status_code = 401
+        response = test_app.get("/metrics/payments/")
+        response_json = response.json()
+
+        assert response.status_code == 401
+        assert response_json["message"] == "Error with API Key"
+
+
+    
